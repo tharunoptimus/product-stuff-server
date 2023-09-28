77816c23270be69c1be243a6e7878b09e583c80b
IN45704543 <Tharunraj.Mailvakanan@tesco.com>
2023-09-28 12:19:58 +0530

    Added ProductSchema

---
+++ schemas/ProductSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const ProductSchema = new Schema({
+    imgSrc: { type: String, required: true, trim: true },
+    quantity: { type: String, required: true, trim: true },
+    price: { type: String, required: true, trim: true },
+    productName: { type: String, required: true, trim: true },
+    productDescription: { type: String, required: true, trim: true },
+    uuid: { type: String, required: true, trim: true, unique: true },
+    addedBy: { type: Schema.Types.ObjectId, ref: "User" },
+})
+
+let Product = mongoose.model('Product', ProductSchema)
+module.exports = Product
