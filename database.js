26b73a75f59724a98ab49afa06de246e74d40206
IN45704543 <Tharunraj.Mailvakanan@tesco.com>
2023-10-04 01:58:48 +0530

    Changed how Database instance is exported (for serverless)

--- database.js
+++ database.js
@@ -1,19 +1,35 @@
 const mongoose = require("mongoose")
-const MongoURI = process.env.MONGODB_URI
-class Database {
+mongoose.set("strictQuery", false)
 
+class Database {
     constructor() {
-        this.connect()
+        this._connect()
     }
 
-    connect() {
-        mongoose.connect(MongoURI)
+    _connect() {
+        mongoose
+            .connect(process.env.MONGODB_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            })
             .then(() => {
-                console.log("Database Connection Successful!")
+                console.log("Database connection successful")
             })
             .catch((err) => {
-                console.log("Database Connection Failed!" + err)
+                console.error("Database connection error")
+                console.log(err)
             })
+    }
+
+    async start() {
+        return new Promise((resolve, reject) => {
+            mongoose.connection.on("connected", () => {
+                resolve()
+            })
+            mongoose.connection.on("error", (err) => {
+                reject(err)
+            })
+        })
     }
 }
 
